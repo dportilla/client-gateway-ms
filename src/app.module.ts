dbd0a59f419fde85425d5904f86d627034b940cd
David Portilla <dportillalara@gmail.com>
2026-04-13 17:00:35 -0400

    feat: implement config module and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { systemConfig } from './config/system.config';
+import { envValidationSchema } from './config/env.validations';
 
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: systemConfig,
+    validationSchema: envValidationSchema
+  })],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+
+	const port = configService.get('app.port');
+
+	const logger = new Logger('Gategway');
+
+	await app.listen(port);
+	logger.log(`Gateway is running on port ${port}`);
 }
 bootstrap();
